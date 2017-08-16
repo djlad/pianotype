881fa15f10a34da94c804f2127d302aa38274abb
dan <dynamicdan100@yahoo.com>
2017-08-16 08:27:33 -0400

    added music and fixed order of notes

--- pianotype/src/app/song-text-display/song-text-display.component.ts
+++ pianotype/src/app/song-text-display/song-text-display.component.ts
@@ -21,16 +21,16 @@ export class SongTextDisplayComponent implements OnInit {
 			The rest of the mappings will be generated 
 		*/
 		let keyboardOrder:string = 'qwertyuiopasdfghjkl;\'zxcvbnm,./';
-		let noteOrder:string[] = [	"A0", "B0", "C0", "D0", "E0", "F0", "G0",
-									"A1", "B1", "C1", "D1", "E1", "F1", "G1",
-									"A2", "B2", "C2", "D2", "E2", "F2", "G2",
-									"A3", "B3", "C3", "D3", "E3", "F3", "G3",
-									"A4", "B4", "C4", "D4", "E4", "F4", "G4",
-									"A5", "B5", "C5", "D5", "E5", "F5", "G5",
-									"A6", "B6", "C6", "D6", "E6", "F6", "G6",
-									"A7", "B7", "C7", "D7", "E7", "F7", "G7",
-									"A8", "B8", "C8", "D8", "E8", "F8", "G8",
-									"A9", "B9", "C9", "D9", "E9", "F9", "G9"]
+		let noteOrder:string[] = [	"C0", "D0", "E0", "F0", "G0", "A0", "B0",  
+									"C1", "D1", "E1", "F1", "G1", "A1", "B1",
+									"C2", "D2", "E2", "F2", "G2", "A2", "B2",
+									"C3", "D3", "E3", "F3", "G3", "A3", "B3",
+									"C4", "D4", "E4", "F4", "G4", "A4", "B4",
+									"C5", "D5", "E5", "F5", "G5", "A5", "B5",
+									"C6", "D6", "E6", "F6", "G6", "A6", "B6",
+									"C7", "D7", "E7", "F7", "G7", "A7", "B7",
+									"C8", "D8", "E8", "F8", "G8", "A8", "B8", 
+									"C9", "D9", "E9", "F9", "G9", "A9", "B9"]
 		let noteIndex:number = noteOrder.indexOf(noteToSetToLetter);
 		let letterIndex:number = keyboardOrder.search(letterToSetToNote);
 		let noteLetterDifference:number = noteIndex - letterIndex;
@@ -47,7 +47,7 @@ export class SongTextDisplayComponent implements OnInit {
 	}
 
 	displayKeyToNoteMap(keyToNotemap){
-		let keyboardOrder:string[] = 'qwertyuiopasdfghijkl;\'zxcvbnm,./'.split('');
+		let keyboardOrder:string[] = 'qwertyuiopasdfghjkl;\'zxcvbnm,./'.split('');
 		let output:string = '';
 		keyboardOrder.map((key)=>{
 			output += key + ': ' + keyToNotemap[key] + '\n';
